8322ed2d36027b8900042ed3fedd53bc17cc564b
Yunus Emre Uzun <yunus.uzun@metu.edu.tr>
2019-12-15 17:39:42 +0300

    retrun val true

--- hw3.py
+++ hw3.py
@@ -19,15 +19,33 @@ class Node:
                 self.type = "variable"
             else:
                 self.type = "constant"
-
     
+    def check_equal(self,clause):
+        if(self.data == clause.data):
+            if(len(self.children) != len(clause.children)):
+                return False
+            elif(len(self.children) == 0):
+                return True
+            else:
+                flag = True
+                for i in range(len(self.children)):
+                    flag = flag and self.children[i].check_equal(clause.children[i])
+                return flag
+        else:
+            return False
 
 
 def theorem_prover(base_clauses, obtained_clauses):
     unification_mapping = unify_clauses(base_clauses)
-    (base_clauses,obtained_clauses) = change_clauses(base_clauses,obtained_clauses,unification_mapping)
-    resolution(base_clauses)
-    return True
+    if(unification_mapping):
+        (base_clauses,obtained_clauses) = change_clauses(base_clauses,obtained_clauses,unification_mapping)
+        for clause in obtained_clauses:
+            base_clauses.append(clause)
+        (res,resolvents) = resolution(base_clauses)
+        if(res):
+            return ("yes", resolvents)
+        else:
+            return ("no", [])
 
 def change_clauses(base_clauses,obtained_clauses,unification_mapping):
     for key in unification_mapping:
@@ -38,8 +56,55 @@ def change_clauses(base_clauses,obtained_clauses,unification_mapping):
     return (base_clauses,obtained_clauses)
 
 def resolution(base_clauses):
-    # Do the resolution
+    new = []
+    while(True):
+        new = []
+        for i in range(len(base_clauses)):
+            for j in range(i+1,len(base_clauses)):
+                resolvents = resolve(base_clauses[i],base_clauses[j])
+                if("" in resolvents):
+                    return (True,base_clauses)
+                else:
+                    new = list(set(new) | set(resolvents))
+        if(len(set(new).difference(set(base_clauses)))==0):
+            return (False,None)
+        else:
+            base_clauses = list(set(new) | set(base_clauses))
+    return None
 
+def resolve(clause_1,clause_2):
+    resolvents = []
+    clause_1 = clause_1.split("+")
+    clause_2 = clause_2.split("+")
+    parsed_clause_1 = parse_function(clause_1)
+    parsed_clause_2 = parse_function(clause_2)
+    for i in parsed_clause_1:
+        for j in parsed_clause_2:
+            if(check_negated(i,j)):
+                c = ""
+                for k in clause_1:
+                    if(parse_function([k])[0].check_equal(i)):
+                        continue
+                    else:
+                        c += "+{}".format(k)
+                for k in clause_2:
+                    if(parse_function([k])[0].check_equal(j)):
+                        continue
+                    else:
+                        c += "+{}".format(k)
+                c = c[1::]
+                resolvents = list(set(resolvents) | set([c]))
+    return resolvents
+
+def check_negated(clause_1,clause_2):
+    if(clause_1.data=="~" and clause_2.data !="~") or (clause_1.data!="~" and clause_2.data=="~"):
+        if(clause_1.data=="~"):
+            clause_1 = clause_1.children[0]
+        else:
+            clause_2 = clause_2.children[0]
+        return clause_1.check_equal(clause_2)
+    else:
+        return False
 
 def extract_functions(base_clauses):
     clause_list = []
@@ -57,7 +122,30 @@ def parse_function(functions):
         root = None
         current_parent = None
         for i in range(len(function)):
-            if(ord(function[i])>96 and ord(function[i])<123): # if it is a lower-case character
+            if(function[i] == "~"):
+                if(root==None):
+                    neg = Node("~","negation",None)
+                    root = neg
+                    current_parent = neg
+                else:
+                    neg = Node("~","negation",current_parent)
+                    current_parent.add_child(neg)
+                    current_parent = neg
+                    
+            elif(function[i] == "+"):
+                if(current_parent.parent == None):
+                    disjunct = Node("+","disjunction", None )
+                    disjunct.add_child(current_parent)
+                    current_parent.parent = disjunct
+                    current_parent = disjunct
+                    root = disjunct
+                else:
+                    disjunct = Node("+","disjunction", current_parent.parent )
+                    current_parent.parent.add_child(disjunct)
+                    disjunct.add_child(current_parent)
+                    current_parent.parent = disjunct
+                    current_parent = disjunct
+            elif(ord(function[i])>96 and ord(function[i])<123): # if it is a lower-case character
                 if(function[i+1]=="("): # it is a function
                     if(root == None): # if it is root
                         root = Node(function[i],"function",None)
@@ -124,4 +212,4 @@ def unify_clauses(base_clauses):
     return unification_mapping
 
 if __name__ == "__main__":
-    theorem_prover(["p(A,f(t))" , "q(z)+~p(z,f(B))" , "~q(y)+r(y)"] , ["~r(A)"])
+    print theorem_prover(["p(A,f(t))" , "q(z)+~p(z,f(B))" , "~q(y)+r(y)"] , ["~r(A)"])
